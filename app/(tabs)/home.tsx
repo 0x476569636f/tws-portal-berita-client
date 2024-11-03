4dff7aac069fd7db60cfa34bf980a5c598e0e2a2
Irvan Pramana Putra <darknet1255@gmail.com>
2024-11-04 00:32:33 +0700

    chore: update ScreenWrapper in Home component to include route name

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -3,7 +3,6 @@ import { Text } from '~/components/nativewindui/Text';
 import { Button } from '~/components/nativewindui/Button';
 import { useAuth } from '~/context/auth';
 import ScreenWrapper from '~/components/ScreenWrapperWithNavbar';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Home() {
   const { user, logout } = useAuth();
@@ -11,7 +10,7 @@ export default function Home() {
   if (!user) return null;
 
   return (
-    <ScreenWrapper>
+    <ScreenWrapper routeName="Beranda">
       <View>
         <Text>Welcome, {JSON.stringify(user)}</Text>
         <Button onPress={logout}>
